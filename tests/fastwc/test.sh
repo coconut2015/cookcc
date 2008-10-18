ff82f4ddc9592f4702c714d64fc4d65432d2e32c
superduperhengyuan <superduperhengyuan@5e97d54e-96ed-11dd-9e5b-cbe0a3a573f3>
2008-10-18 02:43:18 +0000

    change the test script a bit and also add Flex's wc5.l test.

--- tests/fastwc/test.sh
+++ tests/fastwc/test.sh
@@ -8,17 +8,20 @@ do
 
 	$cookcc $v > Lexer.java
 	${JAVA_HOME}/javac Lexer.java > /dev/null 2> /dev/null
-	if [ $? -ne 0 ]; then echo test for $v failed; fi
+	if [ $? -ne 0 ]; then echo test for $v failed; break; fi
 
-	${JAVA_HOME}/java -cp . Lexer bible12.txt > output
-	if [ $? -ne 0 ]; then echo test for $v failed; fi
-	time 	${JAVA_HOME}/java -cp . Lexer bible12.txt > /dev/null
-	time 	${JAVA_HOME}/java -cp . Lexer bible12.txt > /dev/null
-	time 	${JAVA_HOME}/java -cp . Lexer bible12.txt > /dev/null
-	time 	${JAVA_HOME}/java -cp . Lexer bible12.txt > /dev/null
-	time 	${JAVA_HOME}/java -cp . Lexer bible12.txt > /dev/null
+	${JAVA_HOME}/java -cp . Lexer test.input > output
+	if [ $? -ne 0 ]; then echo test for $v failed; break; fi
+	diff output ${v}.output > /dev/null
+	if [ $? -ne 0 ]; then echo test for $v failed; break; fi
+
+	time 	${JAVA_HOME}/java -cp . Lexer test.input > /dev/null
+	time 	${JAVA_HOME}/java -cp . Lexer test.input > /dev/null
+	time 	${JAVA_HOME}/java -cp . Lexer test.input > /dev/null
+	time 	${JAVA_HOME}/java -cp . Lexer test.input > /dev/null
+	time 	${JAVA_HOME}/java -cp . Lexer test.input > /dev/null
+
+	rm -f Lexer.java
+	rm -f Lexer*.class
+	rm -f output
 done
-
-rm -f Lexer.java
-rm -f Lexer*.class
-rm -f output

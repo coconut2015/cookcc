fe9b344142f08ac0ea3cacc83c09d9f9b6651163
superduperhengyuan <superduperhengyuan@5e97d54e-96ed-11dd-9e5b-cbe0a3a573f3>
2008-10-19 15:30:25 +0000

    add CCL test from Flex.

---
+++ trunk/tests/ccl/test.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cookcc="${JAVA_HOME}/java -jar ../../dist/cookcc-1.0.jar"
+
+for v in *.xcc
+do
+	echo testing $v
+
+	$cookcc $v > Lexer.java
+	${JAVA_HOME}/javac Lexer.java > /dev/null 2> /dev/null
+	if [ $? -ne 0 ]; then echo test for $v failed; break; fi
+
+	${JAVA_HOME}/java -cp . Lexer test.input > output
+	if [ $? -ne 0 ]; then echo test for $v failed; break; fi
+	diff output ${v}.output > /dev/null
+	if [ $? -ne 0 ]; then echo test for $v failed; break; fi
+
+	rm -f Lexer.java
+	rm -f Lexer*.class
+	rm -f output
+done

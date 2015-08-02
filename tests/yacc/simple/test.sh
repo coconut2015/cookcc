39d41e00b113cd4d7ede5aab8b91ee7db1e68345
coconut2015 <coconut2015@users.noreply.github.com>
2015-08-02 14:46:35 -0700

    Update the script

--- tests/yacc/simple/test.sh
+++ tests/yacc/simple/test.sh
@@ -7,7 +7,7 @@ for v in *.y
 do
 	echo testing $v
 	echo $v :>>output
-	cookcc $v > /dev/null 2>> output
+	"$java" -jar "${COOKCC}" $v > /dev/null 2>> output
 done
 
 diff output test.output > /dev/null || error test failed
